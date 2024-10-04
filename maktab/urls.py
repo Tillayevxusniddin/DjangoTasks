136456a508276db107df1fbd0f4da11376755ae0
MrPyDev <tillayevx1@gmail.com>
2024-10-04 18:50:32 +0500

    5 task

--- maktab/urls.py
+++ maktab/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('books/', views.book_list, name='book_list'),
+    path('book/<int:book_id>/', views.book_detail, name='book_detail')
 ]
--- maktab/views.py
+++ maktab/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+
 from .models import Book
 
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'book_list.html', {'books': books})
+
+def book_detail(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    return render(request, 'book_detail.html', {'book':book})
+
